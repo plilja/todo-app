1b96b5b8e2509f106db0ef2fba63017d4db23931
plilja <patlil@kth.se>
2017-07-09 19:23:19 +0200

    Add some basic formatting
    Still really ugly, playing a little with Bootstrap

--- todo-spring-boot/src/main/java/se/plilja/todoapp/TodoApplication.java
+++ todo-spring-boot/src/main/java/se/plilja/todoapp/TodoApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableWebMvc
 @Component
 public class TodoApplication implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
