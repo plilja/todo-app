3c368876a74042352656a9dca62a0dd8b507c265
plilja <patlil@kth.se>
2017-07-09 14:37:40 +0200

    Inline ApplicationStartup into Application class

--- todo-spring-boot/src/main/java/se/plilja/todoapp/TodoApplication.java
+++ todo-spring-boot/src/main/java/se/plilja/todoapp/TodoApplication.java
@@ -1,13 +1,28 @@
 package se.plilja.todoapp;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableWebMvc
-public class TodoApplication {
+@Component
+public class TodoApplication implements ApplicationListener<ApplicationReadyEvent> {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        taskRepository.save(new Task("Buy milk", "..."));
+        taskRepository.save(new Task("Clean apartment", "..."));
+        taskRepository.save(new Task("Work out", "..."));
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(TodoApplication.class, args);
