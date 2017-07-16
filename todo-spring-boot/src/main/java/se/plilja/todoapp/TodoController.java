e6d065dd96160123c773f6fd71fd1b0f79d8d878
plilja <patlil@kth.se>
2017-07-16 07:31:46 +0200

    Add page for viewing a task

--- todo-spring-boot/src/main/java/se/plilja/todoapp/TodoController.java
+++ todo-spring-boot/src/main/java/se/plilja/todoapp/TodoController.java
@@ -3,6 +3,7 @@ package se.plilja.todoapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,5 +24,12 @@ public class TodoController {
     public String create(@RequestParam(value = "name", required = true) String name, @RequestParam(value = "description", required = true) String description, Model model) {
         taskRepository.save(new Task(name, description));
         return home("", model);
+    }
+
+    @RequestMapping("/view_task/{taskId}/")
+    public String viewTask(@PathVariable Long taskId, Model model) {
+        Task task = taskRepository.findOne(taskId);
+        model.addAttribute("task", task);
+        return "view_task";
     }
 }
