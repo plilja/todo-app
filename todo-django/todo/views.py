50300c90e63670dd2172e7c787fb21ee91b74607
plilja <patlil@kth.se>
2017-07-18 17:26:59 +0200

    Add index page

--- todo-django/todo/views.py
+++ todo-django/todo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
 
 
 def index(request):
-    return HttpResponse('Hello World!');
+    context = {
+        'tasks': Task.objects.all()
+    }
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
